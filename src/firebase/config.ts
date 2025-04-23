48a9c25933a6bd1130178c53b8bbc9904036951e
João Victor <jvspepe@proton.me>
2025-04-23 17:52:23 -0300

    Initial commit.

---
+++ src/firebase/config.ts
@@ -0,0 +1,20 @@
+import { type FirebaseOptions, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const config: FirebaseOptions = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+};
+
+const app = initializeApp(config);
+const auth = getAuth(app);
+const database = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, database, storage };
