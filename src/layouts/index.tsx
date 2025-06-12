2d01bee36dcf9d93d063e45b6044c784979e9c17
John Victor <jvspepe@proton.me>
2025-06-12 19:10:42 -0300

    Add i18next lib.

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,9 +1,2 @@
-import { Outlet } from 'react-router';
-
-export function Layout() {
-  return (
-    <div className="flex min-h-dvh flex-col items-center justify-center">
-      <Outlet />
-    </div>
-  );
-}
+export { AuthLayout } from './auth-layout';
+export { MainLayout } from './main-layout';
